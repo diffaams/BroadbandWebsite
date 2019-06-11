c08d3bae9405b38609161f84f29dc3807e231415
diffaams <50772534+diffaams@users.noreply.github.com>
2019-06-11 09:44:55 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Coba deh masukin</title>
+</head>
+<body>
+<form method="POST" action="upload.php">
+  <input type="file" name="img" accept="image/*" >
+  <input type="submit" name="Submit" value="upload">
+</form>
+</body>
+</html>
+
+
